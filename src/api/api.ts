d8f4e5c1fb070bef98f89bc4b807c7de772a489f
Arseniy <catdog09073@gmail.com>
2026-03-20 10:10:49 +0300

    refactor: move API keys to environment variables

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
     baseURL: "https://api.coingecko.com/api/v3/",
     params: {
-        x_cg_demo_api_key: "CG-7VEnZGGMJ2dpNmkNBuCiT3L4",
+        x_cg_demo_api_key: import.meta.env.VITE_COINGECKO_API_KEY,
     }
 });
